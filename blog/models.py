8fee287895dd43850db8f44a53613a4e757c6c6b
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2024-11-13 14:23:53 +0000

    Added updated_on Blog models.py

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts") #String, <select name="author" data-context="available-source" required="" id="id_author">
     content = models.TextField() #String, <a class="related-widget-wrapper-link change-related" id="change_id_author" data-href-template="/admin/auth/user/__fk__/change/?_to_field=id&amp;_popup=1" data-popup="yes" title="Change selected user">
     created_on = models.DateTimeField(auto_now_add=True) #String
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0) #Int, <select name="status" id="id_status">
     excerpt = models.TextField(blank=True)
