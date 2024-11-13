6287c5c5b2aa385a137132829dedb48ba5a05118
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2024-11-13 08:54:59 +0000

    Added Poetry and added secrets

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,21 +9,25 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
+import dj_database_url
+from dotenv import load_dotenv, dotenv_values
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Loading variables from .env file
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8axjtfalz)^%b%j@)yz4dh$9%p4&(^j9hln9=q*fe3k#d+cra+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -77,8 +81,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': { dj_database_url.parse(os.getenv('DATABASE_URL'))}
     }
 }
 
