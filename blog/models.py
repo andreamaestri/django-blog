6c6a6823d028ae85dc80ff319841d021082ac3d8
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2024-11-13 16:03:28 +0000

    Added Comments ordering filter models.py

--- blog/models.py
+++ blog/models.py
@@ -14,13 +14,22 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0) #Int, <select name="status" id="id_status">
     excerpt = models.TextField(blank=True)
 
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
     class Meta:
         ordering = ['-created_on']
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        body_excerpt = self.body[:100] + '...' if len(self.body) > 100 else self.body
+        return f"Comment by {self.author}: {body_excerpt}"
+
+    class Meta:
+        ordering = ['created_on']
