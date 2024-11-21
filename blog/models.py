d44bae404074134cb1169526281fb2a8826ce989
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2024-11-21 16:57:34 +0000

    ADD: Integrate Cloudinary for featured image handling in blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -9,6 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True) #String, <input type="text" name="title" class="vTextField" maxlength="200" required="" id="id_title">
     slug = models.SlugField(max_length=200, unique=True) #String, <input type="text" name="slug" class="vTextField" maxlength="200" required="" id="id_slug">
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts") #String, <select name="author" data-context="available-source" required="" id="id_author">
+    featured_image = CloudinaryField('image', default='placeholder')
     content = HTMLField()
     created_on = models.DateTimeField(auto_now_add=True) #String
     updated_on = models.DateTimeField(auto_now=True)
